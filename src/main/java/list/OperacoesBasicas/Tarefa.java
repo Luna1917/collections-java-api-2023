d1dc48cdf51eaf175da03de760e660357119a65d
luna1917 <lun4oliveira@outlook.com>
2023-09-15 19:14:30 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: luna1917 <lun4oliveira@outlook.com>

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,22 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
